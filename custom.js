ea41124c965d96ee3bea24cbfcbcf521452e90ec
jeff <jeff@jeffHome.lan>
2018-04-06 02:54:36 +0200

    first commit

---
+++ custom.js
@@ -0,0 +1,13 @@
+{
+    "git.ignoreMissingGitWarning": true,
+    "editor.fontSize": 18,
+    "editor.fontFamily": "Source Code Pro",
+    "git.autofetch": true,
+    "workbench.startupEditor": "newUntitledFile",
+    "editor.fontWeight": "400",
+    "editor.letterSpacing": 1,
+    "terminal.integrated.fontFamily": "Droid Sans Mono",
+    "terminal.integrated.fontSize": 16,
+    "window.zoomLevel": 0,
+    
+}
